be7e63694b689e678748d6437c3c76eed26be747
scliuyang <mzefibp@163.com>
2016-01-28 15:28:27 +0800

    line-height 完工

---
+++ src/rule/line-height.js
@@ -0,0 +1,32 @@
+/**
+ *  line-height 在定义文本段落时，应使用数值
+ */
+
+var postcss = require('postcss');
+var utils = require('../utils');
+var name = 'line-height';
+var msg = 'Define line-height use number';
+
+module.exports = postcss.plugin(name, function (options) {
+    return function (css, result) {
+
+        var config = options.config;
+        var errorLevel = config[name].level;
+        var notNumberReg = /\d(?=[a-zA-Z%])/;
+
+        css.walkDecls(function (decl) {
+
+            if (decl.prop === 'line-height') {
+
+                if (notNumberReg.test(decl.value)) {
+                    var content = decl.prop + decl.raws.between + decl.value;
+                    result.warn(msg, {
+                        node: decl,
+                        level: errorLevel,
+                        content: content,
+                    });
+                }
+            }
+        });
+    }
+});
