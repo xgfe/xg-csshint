2dcdd73ac5ae0b210b3a2d01b5c542807b4b55ca
scliuyang <mzefibp@163.com>
2016-01-28 13:41:45 +0800

    3.1 [建议] 尽量不使用id选择器定义样式 完成

--- test/rule/not-use-id.spec.js
+++ test/rule/not-use-id.spec.js
@@ -1,5 +1,5 @@
 /**
- * 建议] 如无必要，不得为 id、class 选择器添加类型选择器进行限定
+ *  [建议] 尽量不使用id选择器定义样式
  */
 'use strict';
 
@@ -9,19 +9,20 @@ var path = require('path');
 var getContent = utils.getContent;
 var options = {};
 options.config = require('../../src/config');
-describe("disallow-overqualified-elements", function () {
+
+fdescribe("not-use-id", function () {
     beforeAll(function () {
-        var cssPath = path.join(__dirname, 'css', "disallow-overqualified-elements.css");
+        var cssPath = path.join(__dirname, 'css', "not-use-id.css");
         this.cssString = getContent(cssPath);
         this.cssPath = cssPath;
-        this.messages = csshint(this.cssString, cssPath,options);
+        this.messages = csshint(this.cssString, cssPath, options);
     });
 
     it('messages should be right', function () {
-        expect(this.messages.length).toBe(7);
+        expect(this.messages.length).toBe(1);
     });
 
     it('messages text should be right', function () {
-        expect(this.messages[0].text).toEqual("Not allowed to add a type selector is limited to ID, class selector");
+        expect(this.messages[0].text).toEqual("Do not use the id selector");
     })
 });
